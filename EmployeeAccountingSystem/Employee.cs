fc8d3ee67e122195ad95b36b0cd89b3e26b495ca
Aleksei Shushkov <92529106+DynamicSAV@users.noreply.github.com>
2024-09-26 22:00:09 +0400

    Delete EmployeeAccountingSystem directory

--- EmployeeAccountingSystem/Employee.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EmployeeAccountingSystem;
-
-public abstract class Employee
-{
-  public abstract string Name { get; set; }
-  public abstract decimal BaseSalary { get; }
-  public abstract decimal CalculateSalary();
-}
